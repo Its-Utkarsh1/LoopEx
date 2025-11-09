6a7061dd8371f13a6e19df72d0ff1fe7361e7b54
Utkarsh <utkarsh.sri.9170@gmail.com>
2025-11-09 19:43:36 +0530

    Change MongoDB connection URL variable

--- connect.js
+++ connect.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB connected successfully"))
   .catch(err => console.error("❌ MongoDB connection error:", err))
